8ca43f00b79a434c19d40666378d3addd97a9108
JakeMc2001 <78794919+JakeMc2001@users.noreply.github.com>
2024-01-09 13:44:44 +0000

    adds function to create the character table for given N

---
+++ general/CharacterTable.m
@@ -0,0 +1,10 @@
+function charTable=CharacterTable(N)
+    charTable=zeros(N);
+    m=0:N-1;
+    k=(2*pi*m)/N;
+    t=0:(N-1);
+    for n=1:N
+        charTable(n,:)=exp(1i*k*t(n));
+    end
+
+end
