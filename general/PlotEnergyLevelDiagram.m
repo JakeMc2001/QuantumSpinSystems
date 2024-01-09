3aa43ba87634e1946690dd49e8241dedfb3be675
JakeMc2001 <78794919+JakeMc2001@users.noreply.github.com>
2024-01-09 13:03:00 +0000

    adds functionality with cell array energy list

--- general/PlotEnergyLevelDiagram.m
+++ general/PlotEnergyLevelDiagram.m
@@ -1,12 +1,12 @@
 function PlotEnergyLevelDiagram(EnergyLevels,SystemDescription)
-    xValues=[0 0.5];
-    plot(xValues,[EnergyLevels(1),EnergyLevels(1)]); hold on;
-    for i=2:length(EnergyLevels)
-        plot(xValues,[EnergyLevels(i),EnergyLevels(i)]);
+    % EnergyLevels must be given as a cell array
+    plot(EnergyLevels{1,1}*ones(1,length(EnergyLevels{1,2})),EnergyLevels{1,2},'*'); hold on;
+    for i=2:height(EnergyLevels)
+        plot(EnergyLevels{i,1}*ones(1,length(EnergyLevels{i,2})),EnergyLevels{i,2},'*');
     end
     % y limits can be changed to improve layout of plot
-    %ylim([-2 2]);
-    xlim([0 0.6]); set(gca,'xtick',[]);
-    ylabel('Energy/J');
+    ylim([-2.2 1.2]);
+    xlim([-0.2 2.2]);
+    ylabel('Energy/J'); xlabel('Sz');
     title(["Energy Level Diagram",SystemDescription]);
 end
