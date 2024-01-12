9ce346d3f121ff47bd78ece746225d5776a80d1c
JakeMc2001 <78794919+JakeMc2001@users.noreply.github.com>
2024-01-12 11:57:01 +0000

    adds figure statement to open new plots in separate figure windows

--- general/PlotEnergyLevelDiagram.m
+++ general/PlotEnergyLevelDiagram.m
@@ -1,9 +1,11 @@
 function PlotEnergyLevelDiagram(EnergyLevels,SystemDescription)
     % EnergyLevels must be given as a cell array
+    figure;
     plot(EnergyLevels{1,1}*ones(1,length(EnergyLevels{1,2})),EnergyLevels{1,2},'*'); hold on;
     for i=2:height(EnergyLevels)
         plot(EnergyLevels{i,1}*ones(1,length(EnergyLevels{i,2})),EnergyLevels{i,2},'*');
     end
+    hold off;
     % y limits can be changed to improve layout of plot
     %ylim([-2.2 1.2]);
     %xlim([-0.2 2.2]);
