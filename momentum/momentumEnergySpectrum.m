be41b472919736f4f8e1528dccd6f4c4d5a5c53d
JakeMc2001 <78794919+JakeMc2001@users.noreply.github.com>
2024-02-13 15:23:47 +0000

    returns energy list for system of N spins

---
+++ momentum/momentumEnergySpectrum.m
@@ -0,0 +1,25 @@
+function energyList=momentumEnergySpectrum(N)
+    tic
+    energyList={};
+    index=0;
+    numOfStates=0;
+    for mz=(-N/2):N/2
+        magStates=0;
+        fprintf('  mz = %d\n',mz)
+        for k=0:N-1
+            fprintf('\t k = %0.1f\n',k)
+            tempEnergy=kMagHBlock(N,mz,k);
+            if isempty(tempEnergy)
+                continue
+            end
+            numOfEnergies=length(tempEnergy{2});
+            energyList(index+1,:)=tempEnergy;
+            numOfStates = numOfStates + numOfEnergies;
+            [index,~]=size(energyList);
+            magStates=magStates+numOfEnergies;
+        end
+        fprintf('%d state/s for mz = %d\n',magStates,mz)
+    end
+    fprintf('Total number of states: %d\n',numOfStates)
+    toc
+end
