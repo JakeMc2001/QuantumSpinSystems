3990adfc931e7673f1e54a2c9bae666a15cb1de6
JakeMc2001 <78794919+JakeMc2001@users.noreply.github.com>
2024-02-14 14:42:25 +0000

    updates to ensure correct energies when R<N

--- general/findActiveParents.m
+++ general/findActiveParents.m
@@ -1,10 +1,11 @@
 % k = 0:(N-1) = placeholder momentum
 % actual momentum = k*(pi/2)
 function [ActiveParents,RList]=findActiveParents(N,mz,k)
-    % list of parent states
+    % list of active parent states
     ActiveParents=[];
-    % list of periodicities
+    % list of periodicities of active parents
     RList=[];
+    % iterate over all possible states
     for i=0:(2^N -1)
         % check magnetisation of i
         if getSz(N,i)~=mz
@@ -16,14 +17,14 @@ function [ActiveParents,RList]=findActiveParents(N,mz,k)
         % initialise periodicity R=-1 == not a parent state
         R=-1;
         for g=1:N
-            % find integer of state produced by translating i g times
+            % find integer of state after translating state i g times
             newState=cyclebits(i,g,N);
             if newState < i
                 % i cannot be a parent, move to next i
-                %Mk=0;
                 break
             elseif newState==i
-                %Mk=Mk+charTable(k+1,g-1);
+                % arrived back at the initial state i
+                % break out of for loop over g
                 R=g;
                 break
             end
@@ -31,7 +32,9 @@ function [ActiveParents,RList]=findActiveParents(N,mz,k)
         % i must be a parent state
         % check if i is an active parent
         if R>0
+            % get the orbit of the parent state
             orbit=getOrbit(i,R,N);
+            % check if there is momentum k in the orbit
             if find(orbit(2,:)==k)
                 ActiveParents=[ActiveParents i];
                 RList=[RList R];
