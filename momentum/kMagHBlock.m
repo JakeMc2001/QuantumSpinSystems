f096fbb27d7e48237a92218d404d7089b044de48
JakeMc2001 <78794919+JakeMc2001@users.noreply.github.com>
2024-02-14 14:42:37 +0000

    adds comments

--- momentum/kMagHBlock.m
+++ momentum/kMagHBlock.m
@@ -1,23 +1,36 @@
 function energy=kMagHBlock(N,mz,k)
+    % initialise energy eigenstate cell array
     energy={};
-    index=0;
-    numOfStates=0;
+    % find the active parents for given mz and k
     [s,R]=findActiveParents(N,mz,k);
+    % M = number of active parents
     M=length(s);
+    % output number of active parents
     fprintf('Num of active parents: %d\n',M)
-    numOfStates = numOfStates + M;
+    % initialise H matrix for current k block
     H=zeros(M);
+    % iterate over active parent states
     for a=1:M
+        % sa = current active parent state
         sa=s(a);
+        % get bit representation of sa
         sbits=bitget(sa,N:-1:1);
+        % iterate over spins/bits of parent
         for i=1:N
+            % find position of the next spin
             j=findNextSpin(i+1,N);
+            % check if neighboring spins are parallel
             if sbits(i)==sbits(j)
+                % spins parallel
                 H(a,a)=H(a,a) + 1/4;
             else
+                % spins anti-parallel
                 H(a,a) = H(a,a) - 1/4;
+                % flip spins in positions i and j
                 sflipped = flipSpins(sbits,i,j);
+                % find the parent of the flipped state
                 [l,r]=findLj(sflipped,s);
+                % find position of parent of flipped state
                 b=findState(r,s);
                 if b>=0
                     H(a,b) = H(a,b) + 0.5*(R(a)/R(b))^0.5 *exp(sqrt(-1)*2*pi*k*l/N);
@@ -25,6 +38,7 @@ function energy=kMagHBlock(N,mz,k)
             end
         end
     end
+    % calculate energy eigenvalues
     e=eig(H);
     if isempty(e)
         return
