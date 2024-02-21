94252397f0af011a2fe880c4f83cb9e7a3e0006a
JakeMc2001 <78794919+JakeMc2001@users.noreply.github.com>
2024-02-21 16:39:27 +0000

    adds normalize function which normalizes a given state

--- Lanczos/normalize.m
+++ Lanczos/normalize.m
@@ -1,5 +1,19 @@
-% computes n = <phi|phi>
+% computes n = <phi|phi> = sum_i phi(i)^2
 % then rescales phi so <phi|phi>=1
-function normalize(phi)
-
+% phi is a vector with components i
+function newPhi=normalize(phi)
+    % create variable to store normalised state
+    newPhi=phi;
+    % initialise inner product
+    n=0;
+    % calculate inner product by iterating over components
+    for i=1:length(phi)
+        n = n + phi(i)^2;
+    end
+    % this if statement may need to be changed into a range instead of ==
+    if n==1
+        return
+    end
+    % rescale newPhi so n==1
+    newPhi=phi/sqrt(n);
 end
