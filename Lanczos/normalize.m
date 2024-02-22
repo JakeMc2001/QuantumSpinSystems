809a34a6f66dc84aabf50029de067ba5fb2f7364
JakeMc2001 <78794919+JakeMc2001@users.noreply.github.com>
2024-02-22 10:46:31 +0000

    uses dot() built-in function and returns n

--- Lanczos/normalize.m
+++ Lanczos/normalize.m
@@ -1,15 +1,17 @@
 % computes n = <phi|phi> = sum_i phi(i)^2
 % then rescales phi so <phi|phi>=1
 % phi is a vector with components i
-function newPhi=normalize(phi)
+function [newPhi,n]=normalize(phi)
     % create variable to store normalised state
     newPhi=phi;
+    % calculate inner product
+    n=dot(phi,phi);
     % initialise inner product
-    n=0;
+    %n=0;
     % calculate inner product by iterating over components
-    for i=1:length(phi)
-        n = n + phi(i)^2;
-    end
+    %for i=1:length(phi)
+    %    n = n + phi(i)^2;
+    %end
     % this if statement may need to be changed into a range instead of ==
     if n==1
         return
